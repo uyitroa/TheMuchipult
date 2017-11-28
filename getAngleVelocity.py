7c2aa5357d081419a6a9f1713e68d367fee97214
Shingatsu <30695483+Shingatsu@users.noreply.github.com>
2017-11-28 10:05:14 +0100

    Update getAngleVelocity.py

--- getAngleVelocity.py
+++ getAngleVelocity.py
@@ -5,28 +5,30 @@ def WaitUntilClicked(window):
 		decideToPull = False
 
 		while decideToPull == False:
-
-			if window.checkMouse() != None:
-				xMouse = window.checkMouse().getX()
-				yMouse = window.checkMouse().getY()
+			coordMouse = window.checkMouse()
+			
+			if coordMouse != None:
+				xMouse =  coordMouse.getX()
+				yMouse = coordMouse.getY()
 
 				if almostEqual(xMouse,xRock) and almostEqual(yMouse,yRock):
 					decideToPull = True
 
 def almostEqual(compared, comparee):
-	if compared >= comparee - 5 and compared <= comparee +5:
-		return True
-	else:
-		return False
-
-
-def pullSystem(rock,window):
+	"""Function to check if two values are almost equal 
+	(means a difference of 5 unit)"""
 	
+	return compared >= comparee - 5 and compared <= comparee +5
+
+
+def pullSystem(rock,xRock,yRock,window):
+	"""Funtion to animate well player drag the rock to fire
+	rock will follow the cursor and etc"""
 	# Rock need to follow the cursor
-	xRock = window.checkMouse().getX()
-	yRock = window.checkMouse().getY()
-	rock = Circle(Point(xRock,yRock),5)
-	rock.draw(window)
+	xMouse = window.checkMouse().getX()
+	yMouse = window.checkMouse().getY()
+
+	rock.move(xMouse - xRock, yMouse - yRock)
 
 	connectLine = Line(Point(xRock,yRock),Point(210,620))
 	connectLine.draw(window)
