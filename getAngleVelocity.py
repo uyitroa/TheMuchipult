989263e042cb5e5396179b1c45b560f6f8039b31
Shingatsu <30695483+Shingatsu@users.noreply.github.com>
2017-12-03 21:49:03 +0100

    Update getAngleVelocity.py

--- getAngleVelocity.py
+++ getAngleVelocity.py
@@ -1,17 +1,25 @@
 from graphics import *
 
 
-def WaitUntilClicked(window):
+def motion(event):
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
+
+
+def WaitUntilClicked(window,xRock,yRock):
 		"""wait until player decide to 
 		fire the rock"""
 		decideToPull = False
 
 		while not decideToPull:
+			window.bind('<Motion>', motion)
 			coordMouse = window.checkMouse()
 			
 			if coordMouse != None:
 				xMouse =  coordMouse.getX()
 				yMouse = coordMouse.getY()
+				print(xMouse)
+				print(yMouse)
 
 				if almostEqual(xMouse,xRock) and almostEqual(yMouse,yRock):
 					decideToPull = True
@@ -27,8 +35,8 @@ def pullSystem(rock,xRock,yRock,window):
 	"""Funtion to animate well player drag the rock to fire
 	rock will follow the cursor and etc"""
 	# Rock need to follow the cursor
-	xMouse = window.checkMouse().getX()
-	yMouse = window.checkMouse().getY()
+	"""xMouse = window.checkMouse().getX()
+	yMouse = window.checkMouse().getY()"""
 
 	rock.move(xMouse - xRock, yMouse - yRock)
 
@@ -39,20 +47,20 @@ def pullSystem(rock,xRock,yRock,window):
 
 
 def main():
-	win = GraphWin("My Circle", 1440, 900)
+	win = GraphWin("My Circle", 900, 600)
 
 	
-	Stand = Rectangle(Point(220,800),Point(200,600))
+	Stand = Rectangle(Point(220,500),Point(200,300))
 	Stand.draw(win)
 	
 
 	xRock = 210
-	yRock = 620
+	yRock = 320
 
 	rock = Circle(Point(xRock,yRock),5)
 	rock.draw(win)
 
-	WaitUntilClicked(win)			
+	WaitUntilClicked(win,xRock, yRock)			
 	
 	while True:
 		xRock, yRock = pullSystem(rock,xRock,yRock,win)
