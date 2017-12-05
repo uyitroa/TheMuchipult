51736bdfc1490fef1d7b4a19e774fd9a20268259
Shingatsu <30695483+Shingatsu@users.noreply.github.com>
2017-12-05 23:18:03 +0100

    Update getAngleVelocity.py

--- getAngleVelocity.py
+++ getAngleVelocity.py
@@ -1,69 +1,112 @@
 from graphics import *
+import math
 
+def motion(root):
+	"""get mouse coordinate even not clicked"""
 
-def motion(event):
-    x, y = event.x, event.y
-    print('{}, {}'.format(x, y))
+	x = root.winfo_pointerx() - root.winfo_rootx()
+	y = root.winfo_pointery() - root.winfo_rooty()
+	return x,y
 
+def WaitUntilClicked(window, xRock, yRock):
+	"""wait until player decide to
+		fire the rock
+		:type window: object"""
+	decideToPull = False
 
-def WaitUntilClicked(window,xRock,yRock):
-		"""wait until player decide to 
-		fire the rock"""
-		decideToPull = False
+	while not decideToPull:
 
-		while not decideToPull:
-			window.bind('<Motion>', motion)
-			coordMouse = window.checkMouse()
-			
-			if coordMouse != None:
-				xMouse =  coordMouse.getX()
-				yMouse = coordMouse.getY()
-				print(xMouse)
-				print(yMouse)
+		coordMouse = window.checkMouse()
 
-				if almostEqual(xMouse,xRock) and almostEqual(yMouse,yRock):
-					decideToPull = True
+		if coordMouse != None:
+			xMouse = coordMouse.getX()
+			yMouse = coordMouse.getY()
+
+			if almostEqual(xMouse, xRock) and almostEqual(yMouse, yRock):
+				decideToPull = True
+
 
 def almostEqual(compared, comparee):
 	"""Function to check if two values are almost equal 
 	(means a difference of 5 unit)"""
-	
-	return compared >= comparee - 5 and compared <= comparee +5
+
+	return comparee - 5 <= compared <= comparee + 5
 
 
-def pullSystem(rock,xRock,yRock,window):
+def pullSystem(connectLine,connectLine2,rock, xRock, yRock, window):
 	"""Funtion to animate well player drag the rock to fire
 	rock will follow the cursor and etc"""
+
 	# Rock need to follow the cursor
+
 	"""xMouse = window.checkMouse().getX()
 	yMouse = window.checkMouse().getY()"""
 
+	xMouse, yMouse = motion(window)
+
 	rock.move(xMouse - xRock, yMouse - yRock)
 
-	connectLine = Line(Point(xRock,yRock),Point(210,620))
+
+	connectLine.undraw()
+	window.update()
+
+	connectLine2.undraw()
+	window.update()
+
+
+	connectLine = Line(Point(xRock, yRock), Point(210, 320))
+	connectLine2 = Polygon(Point(xRock, yRock), Point(210, 310))
 	connectLine.draw(window)
-	
-	return xRock,yRock
+	connectLine2.draw(window)
 
 
-def main():
-	win = GraphWin("My Circle", 900, 600)
+	return xMouse, yMouse, connectLine, connectLine2 # return mouse coordinate for the new coord of rock
 
-	
-	Stand = Rectangle(Point(220,500),Point(200,300))
+
+def calculValues(xRock, yRock):
+
+	opposite_side = yRock - 300
+	hypotenuse = math.sqrt((220 - xRock)**2 + (300 - yRock)**2)
+
+	angle = math.asin(opposite_side/hypotenuse) # radian type
+	angle = math.degrees(angle)
+
+	velocity = hypotenuse/100
+
+	return xRock, yRock, velocity, angle
+
+def getValues(win):
+	#win = GraphWin("My Circle", 900, 600)
+
+	Stand = Rectangle(Point(220, 500), Point(200, 300))
 	Stand.draw(win)
-	
 
 	xRock = 210
 	yRock = 320
 
-	rock = Circle(Point(xRock,yRock),5)
+	rock = Circle(Point(xRock, yRock), 5)
 	rock.draw(win)
 
-	WaitUntilClicked(win,xRock, yRock)			
-	
+	connectLine = Polygon(Point(xRock , yRock),Point(210,320))
+	connectLine2 = Polygon(Point(xRock, yRock),Point(210,310))
+
+
+	WaitUntilClicked(win, xRock, yRock)
+
+
 	while True:
-		xRock, yRock = pullSystem(rock,xRock,yRock,win)
+		xRock, yRock, connectLine, connectLine2 = pullSystem(connectLine, connectLine2,rock, xRock, yRock, win)
+		if win.checkMouse():
+			break
+
+	return calculValues(xRock, yRock)
+
 
 if __name__ == "__main__":
-	main()
+	win = GraphWin("My Circle", 900, 600)
+	x,y,velo,angle = getValues(win)
+	print("x_Rock:",x)
+	print("y_Rock:",y)
+	print("Velocity:",velo)
+	print("Angle:",angle)
+
