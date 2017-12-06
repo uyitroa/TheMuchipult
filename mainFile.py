b03443ed0cd9a08b1c71da7798b95563066321ed
Shingatsu <30695483+Shingatsu@users.noreply.github.com>
2017-12-06 19:48:08 +0100

    Create mainFile.py

---
+++ mainFile.py
@@ -0,0 +1,15 @@
+import getAngleVelocity # <- my part
+import CalculateCatapults # <- your part
+
+def frames():
+	win = GraphWin("Catapult", 900, 600)
+	Stand = Rectangle(Point(220, 500), Point(200, 300))
+	Stand.draw(win)
+
+	x, y, velocity, angle = getAngleVelocity.getAngle(win)
+
+	"""x, y = coord of rock"""
+	"""variable win is the window that we will draw/work on"""
+	CalculateCatapults.main(win, x, y, velocity, angle) # <- your part
+
+frames()
